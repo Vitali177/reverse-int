1ba827be943b211f8b29a1220285f229818d9a40
Vitali <vskrabatun@gmail.com>
2020-02-19 21:40:24 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = -n;
+        return +(`${n}`.split('').reverse().join(''));
+    }
+    return +(`${n}`.split('').reverse().join(''));
 }
